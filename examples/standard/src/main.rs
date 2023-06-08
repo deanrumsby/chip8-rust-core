7cde8028c4debe4f68fc4e15f8df28f16a23bfce
Dean Rumsby <dean@deanrumsby.com>
2023-06-08 17:46:40 +0100

    feat: add key maps to standard example

--- examples/standard/src/main.rs
+++ examples/standard/src/main.rs
@@ -1,15 +1,17 @@
-use chip8_core::Chip8;
-use pixels::{Pixels, SurfaceTexture, Error};
+use chip8_core::{Chip8, Key, KeyState};
+
+use pixels::{Error, Pixels, SurfaceTexture};
 use winit::{
-    event::{Event, WindowEvent},
+    event::{ElementState, Event, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
 
-use std::path::Path;
-use std::fs;
-use std::env;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::{
+    env, fs,
+    path::Path,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 fn main() -> Result<(), Error> {
     let event_loop = EventLoop::new();
@@ -17,15 +19,12 @@ fn main() -> Result<(), Error> {
         .with_title("Chip8")
         .with_inner_size(winit::dpi::LogicalSize::new(640, 320))
         .with_resizable(false)
-        .build(&event_loop).unwrap();
+        .build(&event_loop)
+        .unwrap();
 
     let mut pixels = {
         let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(
-            window_size.width,
-            window_size.height,
-            &window
-        );
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
 
         Pixels::new(64, 32, surface_texture)?
     };
@@ -44,12 +43,49 @@ fn main() -> Result<(), Error> {
                 event: WindowEvent::CloseRequested,
                 window_id,
             } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-            
+
+            Event::WindowEvent {
+                event: WindowEvent::KeyboardInput { input, .. },
+                window_id,
+            } if window_id == window.id() => {
+                let key_code = match input.virtual_keycode {
+                    Some(key) => match key {
+                        VirtualKeyCode::Key1 => Some(Key::Key1),
+                        VirtualKeyCode::Key2 => Some(Key::Key2),
+                        VirtualKeyCode::Key3 => Some(Key::Key3),
+                        VirtualKeyCode::Key4 => Some(Key::KeyC),
+                        VirtualKeyCode::Q => Some(Key::Key4),
+                        VirtualKeyCode::W => Some(Key::Key5),
+                        VirtualKeyCode::E => Some(Key::Key6),
+                        VirtualKeyCode::R => Some(Key::KeyD),
+                        VirtualKeyCode::A => Some(Key::Key7),
+                        VirtualKeyCode::S => Some(Key::Key8),
+                        VirtualKeyCode::D => Some(Key::Key9),
+                        VirtualKeyCode::F => Some(Key::KeyE),
+                        VirtualKeyCode::Z => Some(Key::KeyA),
+                        VirtualKeyCode::X => Some(Key::Key0),
+                        VirtualKeyCode::C => Some(Key::KeyB),
+                        VirtualKeyCode::V => Some(Key::KeyF),
+                        _ => None,
+                    },
+                    None => None,
+                };
+                let key_state = match input.state {
+                    ElementState::Pressed => KeyState::Pressed,
+                    ElementState::Released => KeyState::Released,
+                };
+
+                match key_code {
+                    Some(key) => chip8.handle_key_event(key, key_state),
+                    None => (),
+                }
+            }
+
             Event::MainEventsCleared => {
                 chip8.emulate(current_time());
                 pixels.frame_mut().copy_from_slice(chip8.frame());
                 pixels.render().unwrap();
-            },
+            }
             _ => (),
         }
     });
