5c02b4e3d54d891e0c857f5c87056c2f83e2ed20
Dean Rumsby <dean@deanrumsby.com>
2023-06-21 16:02:17 +0100

    fix: correct publish script

---
+++ publish.zsh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+# build npm package
+wasm-pack build --scope deanrumsby -- --features wasm &&
+
+# dry run for crates.io
+cargo publish --features std --dry-run &&
+
+# dry run for npm
+cd pkg &&
+npm publish --dry-run &&
+
+# publish to npm
+npm publish &&
+
+# publish to crates.io
+cd ../ &&
+cargo publish --features std &&
+
+git tag -a $1 -m "Release $1" &&
+git push --tags &&
+echo "Published $1"
